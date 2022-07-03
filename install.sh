5b498cafc764ff804467877e38cd0ac165118c43
Allximik <allximik50@gmail.com>
2022-07-03 16:58:58 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ docker-compose down && docker-compose up -d
 cd ../../
 
 docker-compose down && docker-compose build --no-cache
+
+docker-compose up -d
+
+git clone git@github.com:quintegro/CampingWorld-RVRentals-Web.git www
+docker exec -it php bash
